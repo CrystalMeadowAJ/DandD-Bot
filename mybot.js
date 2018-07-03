2dc3fa1b277d7fde973c0b0fe4d5e0973bf69476
CrystalMeadow <CrystalMeadowAJ@users.noreply.github.com>
2018-07-03 21:30:05 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,13 +1,47 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const prefix = "!";
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+
+  # Ignore other bots
+  if (message.author.bot) return;
+
+  # Only process commands beginning with the set prefix
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  # Split input into commands and arguments
+  const args = message.substring(prefix.length).trim().split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (comamnd === "ping") {
     message.channel.send("pong!");
+  }
+
+  if (command === "roll") {
+    if (args.length < 1) {
+      message.channel.send("'Roll' requires one argument");
+      return;
+    }
+    var argRegex = /(\d*)d(\d*)/i;
+    var match = argRegex.exec(args[0]);
+    if (match.length !== 2) {
+      message.channel.send("Incorrect command format");
+      return;
+    }
+    # Should probably put limits on these
+    var x = match[0];
+    var y = match[1];
+    var results = [];
+    for(var i = 0; i < x; i++) {
+      results.push(Math.floor(Math.random() * (y - min + 1)) + min);
+    }
+    message.channel.send("Rolled the following numbers: " + results.join(", "));
   }
 });
 
